6d6c2aab90c48ad5b04f1af0db4bc36f8eb48e62
emrekuecuek <ekucuk19@ku.edu.tr>
2020-04-29 21:10:41 +0300

    momentum-experiments-added

--- src/SM3.jl
+++ src/SM3.jl
@@ -1,12 +1,9 @@
 module SM3
-
-export
-    SM3
-    sm3
-    sm3!
     
 using Knet, LinearAlgebra, AutoGrad: full
 include("optimizer.jl")
 include("helperfunctions.jl")
+
+export SM3, sm3, sm3!
 
 end
